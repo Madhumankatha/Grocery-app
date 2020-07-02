f94913be76927f0e25b01a452ef8359f0ffce96b
Madhumankatha <madhumankatha@gmail.com>
2020-07-02 13:10:53 +0530

    integrated with recyclerview

---
+++ app/src/main/java/com/madhumankatha/groceryapp/Item.java
@@ -0,0 +1,24 @@
+package com.madhumankatha.groceryapp;
+
+public class Item {
+
+    private long id;
+
+    private String itemName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+}
